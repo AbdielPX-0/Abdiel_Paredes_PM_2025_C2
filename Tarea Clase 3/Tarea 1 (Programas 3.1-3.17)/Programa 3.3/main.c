1f7685ca0acb273265c5c4a5ce7ba7f2132a3da3
Abdiel Paredes <abdielparedes633@gmail.com>
2025-06-05 21:14:12 -0400

    Agrego Tarea 1: Problemas 3.1-1.17

---
+++ Tarea Clase 3/Tarea 1 (Programas 3.1-3.17)/Programa 3.3/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* Suma pagos.
+El programa, al recibir como datos un conjunto de pagos realizados en el ultimo
+mes, obtiene la suma de los mismos.
+
+PAG y Spa: variables de tipo real */
+
+void main (void)
+{
+    float PAG, SPA;
+    SPA = 0;
+    printf("Ingrese el primer pago: \t");
+    scanf("%f", &PAG);
+    while (PAG)
+        /* Observa que la condicion es verdadera mientras el pago es diferente de cero. */
+    {
+        SPA = SPA + PAG;
+        printf("Ingrese el siguiente pago:\t ");
+        scanf("%f", &PAG);
+        /* Observa que la proposicion que modifica condicion es una lectura. */
+
+    }
+        printf("\nEl total de pafos del mes es: %.2f", SPA);
+    }
